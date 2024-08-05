12fdb56012a4572dfa7a92cc8e9176f12acce1ed
nhatvy1 <vyhuynhak123@gmail.com>
2024-08-05 23:17:20 +0700

    upload

--- src/components/Widget/BackToTop.tsx
+++ src/components/Widget/BackToTop.tsx
@@ -1,7 +1,6 @@
 'use client'
 
 import { useEffect, useState } from 'react'
-import IconArrow from '../icons/IconArrow'
 import { MdOutlineKeyboardArrowUp } from 'react-icons/md'
 
 const BackToTop = () => {
